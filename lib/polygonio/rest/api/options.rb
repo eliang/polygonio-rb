3b5d49b6ebec76149434d84ca2829bc49ddfa2d2
Edwin Liang <edwin@swiftmedical.io>
2024-07-16 15:04:57 -0400

    Added aggregates, daily open close

--- lib/polygonio/rest/api/options.rb
+++ lib/polygonio/rest/api/options.rb
@@ -8,7 +8,7 @@ module Polygonio
           attribute :cfi, Types::String
           attribute :contract_type, Types::String
           attribute :exercise_style, Types::String
-          attribute :expiration_date, Types::String
+          attribute :expiration_date, Types::JSON::DateTime
           attribute :primary_exchange, Types::String
           attribute :shares_per_contract, Types::Integer
           attribute :strike_price, Types::JSON::Decimal
@@ -23,6 +23,57 @@ module Polygonio
         res = client.request.get("v3/reference/options/contracts/O:#{ticker}")
 
         ContractResponse[res.body]
+      end
+
+      class DailyOpenCloseResponse < PolygonResponse
+        attribute :status, Types::String
+        attribute :symbol, Types::String
+        attribute :open, Types::JSON::Decimal
+        attribute :high, Types::JSON::Decimal
+        attribute :low, Types::JSON::Decimal
+        attribute :close, Types::JSON::Decimal
+        attribute :volume, Types::Integer
+        attribute :after_hours, Types::JSON::Decimal
+        attribute :from, Types::JSON::DateTime
+      end
+
+      def daily_open_close(symbol, date)
+        symbol = Types::String[symbol]
+        date = Types::JSON::Date[date]
+
+        res = client.request.get("/v1/open-close/O:#{symbol}/#{date}")
+        DailyOpenCloseResponse[res.body]
+        end
+
+      class AggregatesResponse < PolygonResponse
+        attribute :ticker, Types::String
+        attribute :status, Types::String
+        attribute :adjusted, Types::Bool
+        attribute :query_count, Types::Integer
+        attribute :results_count, Types::Integer
+        attribute :results, Types::Array do
+          attribute? :T, Types::String # Not appearing
+          attribute :v, Types::JSON::Decimal
+          attribute? :vw, Types::JSON::Decimal
+          attribute :o, Types::JSON::Decimal
+          attribute :c, Types::JSON::Decimal
+          attribute :h, Types::JSON::Decimal
+          attribute :l, Types::JSON::Decimal
+          attribute :t, Types::Integer
+          attribute? :n, Types::Integer
+        end
+      end
+
+      def aggregates(ticker, multiplier, timespan, from, to, unadjusted = false) # rubocop:disable Metrics/ParameterLists
+        ticker = Types::String[ticker]
+        multiplier = Types::Integer[multiplier]
+        timespan = Types::Coercible::String.enum("minute", "hour", "day", "week", "month", "quarter", "year")[timespan]
+        from = Types::JSON::Date[from]
+        to = Types::JSON::Date[to]
+        unadjusted = Types::Bool[unadjusted]
+
+        res = client.request.get("/v2/aggs/ticker/O:#{ticker}/range/#{multiplier}/#{timespan}/#{from}/#{to}", { unadjusted: unadjusted })
+        AggregatesResponse[res.body]
       end
     end
   end
--- test/rest/options_test.rb
+++ test/rest/options_test.rb
@@ -13,4 +13,20 @@ class OptionsTest < Minitest::Test
       assert_equal 650, res.results.strike_price
     end
   end
+
+  def test_daily_open_close
+    VCR.use_cassette("options_daily_open_close") do
+      ticker = 'SPY251219C00650000'
+      res = @client.options.daily_open_close(ticker, '2024-07-15')
+      assert_equal ticker, res.symbol.gsub('O:', '')
+    end
+  end
+
+  def test_aggregates
+    VCR.use_cassette("options_aggregates") do
+      ticker = 'SPY251219C00650000'
+      res = @client.options.aggregates(ticker, 1, :day, "2024-07-01", "2024-07-05")
+      assert_equal 2, res.results_count
+    end
+  end
 end
